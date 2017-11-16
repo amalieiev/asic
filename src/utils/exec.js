4f47358c5dde9d14e76cabc0092d918d441481c3
artem.malieiev <artem.malieiev@gmail.com>
2017-11-16 12:27:40 +0200

    fixed bug

--- src/utils/exec.js
+++ src/utils/exec.js
@@ -1,9 +1,9 @@
 export function $exec(expression, context) {
     for (let key in context) {
         if (context[key].toUpperCase) {
-            Function(`key = "${context[key]}"`)()
+            Function(`${key} = "${context[key]}"`)()
         } else {
-            Function(`key = ${context[key]}`)()
+            Function(`${key} = ${context[key]}`)()
         }
     }
 
