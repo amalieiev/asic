220f43ec7d0b67a7ca132eb5f875c6f2a2d255a4
artem.malieiev <artem.malieiev@gmail.com>
2018-07-27 09:27:16 +0300

    updated version

--- docs/Header.js
+++ docs/Header.js
@@ -9,7 +9,7 @@ import './stylesheets/stylesheet.css'
 
     <h2 class="project-tagline">Progressive JavaScript Framework</h2>
     <a href="https://github.com/amalieiev/asic" class="btn">View on GitHub</a>
-    <a href="https://jsfiddle.net/amalieiev/171kzqL6/" class="btn">Try on JSFiddle</a>
+    <a href="https://jsfiddle.net/amalieiev/zdmq82ce/" class="btn">Try on JSFiddle</a>
     <a href="https://github.com/amalieiev/asic/zipball/master" class="btn">Download .zip</a>
 </section>
     `
