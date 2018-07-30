2055f143c876d09634b20f8c490802656c218b2b
artem.malieiev <artem.malieiev@gmail.com>
2018-07-30 09:55:37 +0300

    fixed docs

--- docs/Footer.js
+++ docs/Footer.js
@@ -3,7 +3,7 @@
 <section class="main-content">
     <footer class="site-footer">
         <span class="site-footer-owner"><a href="https://github.com/amalieiev/asic">{{ title }}</a> is maintained by <a
-                    href="https://github.com/amalieiev">{{ author }}</a>.</span>
+                    href="https://github.com/amalieiev">{{ author }}</a>. Our other libraries <a href="https://amalieiev.github.io/vue-crosstab" target="_blank" style="color: #159957;">Vue Crosstab</a>.</span>
     </footer>
 </section>
     `
--- docs/Header.js
+++ docs/Header.js
@@ -14,8 +14,8 @@ import './stylesheets/stylesheet.css'
         <img src="https://camo.githubusercontent.com/d0e25b09a82bc4bfde9f1e048a092752eebbb4f3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6c6963656e73652d4d49542d626c75652e7376673f7374796c653d666c6174" alt="License" data-canonical-src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat">
     </div>
 
-    <a href="https://github.com/amalieiev/asic" class="btn">View on GitHub</a>
-    <a href="https://jsfiddle.net/amalieiev/zdmq82ce/" class="btn">Try on JSFiddle</a>
+    <a href="https://github.com/amalieiev/asic" target="_blank" class="btn">View on GitHub</a>
+    <a href="https://jsfiddle.net/amalieiev/zdmq82ce/" target="_blank" class="btn">Try on JSFiddle</a>
     <a href="https://github.com/amalieiev/asic/zipball/master" class="btn">Download .zip</a>
     
     <div class="row">
